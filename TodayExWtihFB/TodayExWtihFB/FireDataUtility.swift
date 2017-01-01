a7a98f5f9d59476c2db00cac8bc6f12a32b53319
Perry <chenzhipeng901105@gmail.com>
2017-01-01 11:45:25 +0800

    ddd

---
+++ TodayExWtihFB/TodayExWtihFB/FireDataUtility.swift
@@ -0,0 +1,13 @@
+//
+//  FireDataUtility.swift
+//  TodayExWtihFB
+//
+//  Created by Perry Z Chen on 1/1/17.
+//  Copyright © 2017 Perry Z Chen. All rights reserved.
+//
+
+import UIKit
+
+class FireDataUtility: NSObject {
+
+}
